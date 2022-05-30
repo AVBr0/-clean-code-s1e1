f0029e3dcb7d4c8292ef5d14412b0a9259c8acdd
avbr0 <logweb01@gmail.com>
2022-05-31 01:40:47 +0300

    refactor: replace tag-selectors according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ var createNewTaskElement=function(taskString){
 
     //Each elements, needs appending
     checkBox.type="checkbox";
-    editInput.type="text";
-    editInput.className="task";
+    editInput.className="task task__text";
+    editInput.className="task task__text";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
@@ -82,7 +82,7 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
+    var editInput=listItem.querySelector('.task__text');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
     var containsClass=listItem.classList.contains("editMode");
